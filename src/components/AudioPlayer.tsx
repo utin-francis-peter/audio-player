e66b309a0e5477da761403150574b61fc7b0c9cb
utin-francis-peter <u.francispeter@gmail.com>
2023-04-26 10:38:28 +0100

    chore: attached useRef to ReactPlayer component for accessing properties and methods between renders, w/o causing re-render of AudioPlayer comp.

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,3 +1,21 @@
-export const AudioPlayer = () => {
-  return <div></div>;
+// components/AudioPlayer.tsx
+
+import ReactPlayer from "react-player";
+import { useRef } from "react";
+
+type Props = {
+  url: string;
+  title: string;
+  author: string;
+  thumbnail: string;
+};
+
+export const AudioPlayer = ({ url, title, author, thumbnail }: Props) => {
+  const playerRef = useRef<ReactPlayer | null>(null);
+
+  return (
+    <div>
+      <ReactPlayer ref={playerRef} url={url} />
+    </div>
+  );
 };
